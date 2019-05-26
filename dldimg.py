ccd21bf8856e8e5ac9cd05aa62ed6fb570efa800
Feekoos <filip.petrinjak@gmail.com>
2019-05-26 14:48:08 +0200

    Add files via upload

---
+++ dldimg.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env/python
+
+import Image, urllib, StringIO
+from math import log, exp, tan, atan, pi, ceil, cos
+from geomath import meters2lat, meters2lon, dm2decdeg, dms2decdeg, checkformat
+from geomath import convert2decdeg
+
+EARTH_RADIUS = 6378137.0
+EQUATOR_CIRCUMFERENCE = 2 * pi * EARTH_RADIUS
+INITIAL_RESOLUTION = EQUATOR_CIRCUMFERENCE / 256.0
+ORIGIN_SHIFT = EQUATOR_CIRCUMFERENCE / 2.0
+
+def latlontopixels(lat, lon, zoom):
+    mx = (lon * ORIGIN_SHIFT) / 180.0
+    my = log(tan((90 + lat) * pi/360.0))/(pi/180.0)
+    my = (my * ORIGIN_SHIFT) /180.0
+    res = INITIAL_RESOLUTION / (2**zoom)
+    px = (mx + ORIGIN_SHIFT) / res
+    py = (my + ORIGIN_SHIFT) / res
+    return px, py
+
+def pixelstolatlon(px, py, zoom):
+    res = INITIAL_RESOLUTION / (2**zoom)
+    mx = px * res - ORIGIN_SHIFT
+    my = py * res - ORIGIN_SHIFT
+    lat = (my / ORIGIN_SHIFT) * 180.0
+    lat = 180 / pi * (2*atan(exp(lat*pi/180.0)) - pi/2.0)
+    lon = (mx / ORIGIN_SHIFT) * 180.0
+    return lat, lon
+
+############################################
+
+lat = raw_input('Please enter lat: ').replace(',', '.').split()
+lon = raw_input('Please enter lon: ').replace(',', '.').split()
+height = int(raw_input('Please enter height [m]: '))
+width = int(raw_input('Please enter width [m]: '))
+zoom = int(raw_input('Please enter zoom (1-19): '))
+
+lat, lon = convert2decdeg(lat, lon)
+
+upperlat, lowerlat = meters2lat(lat, height)
+upperlat, lowerlat = str(upperlat), str(lowerlat)
+
+rightlon, leftlon = meters2lon(lat, lon, width)
+rightlon, leftlon = str(rightlon), str(leftlon)
+
+upperleft = upperlat + ',' + leftlon
+lowerright = lowerlat + ',' + rightlon
+
+print ("Upper left corner: " + upperleft)
+print ("Lower right corner: " + lowerright)
+############################################
+
+ullat, ullon = map(float, upperleft.split(','))
+lrlat, lrlon = map(float, lowerright.split(','))
+
+# Set some important parameters
+scale = 1
+maxsize = 640
+
+# convert all these coordinates to pixels
+ulx, uly = latlontopixels(ullat, ullon, zoom)
+lrx, lry = latlontopixels(lrlat, lrlon, zoom)
+
+# calculate total pixel dimensions of final image
+dx, dy = lrx - ulx, uly - lry
+
+# calculate rows and columns
+cols, rows = int(ceil(dx/maxsize)), int(ceil(dy/maxsize))
+print ("Downloading %d images." % (cols * rows))
+# calculate pixel dimensions of each small image
+bottom = 120
+largura = int(ceil(dx/cols))
+altura = int(ceil(dy/rows))
+alturaplus = altura + bottom
+
+final = Image.new("RGB", (int(dx), int(dy)))
+for x in range(cols):
+    for y in range(rows):
+        dxn = largura * (0.5 + x)
+        dyn = altura * (0.5 + y)
+        latn, lonn = pixelstolatlon(ulx + dxn, uly - dyn - bottom/2, zoom)
+        position = ','.join((str(latn), str(lonn)))
+        print (x, y, position)
+        urlparams = urllib.urlencode({'center': position,
+                                      'zoom': str(zoom),
+                                      'size': '%dx%d' % (largura, alturaplus),
+                                      'maptype': 'satellite',
+                                      'sensor': 'false',
+                                      'scale': scale})
+        url = 'http://maps.google.com/maps/api/staticmap?' + urlparams
+        f=urllib.urlopen(url)
+        im=Image.open(StringIO.StringIO(f.read()))
+        final.paste(im, (int(x*largura), int(y*altura)))
+final.save("tmp-dldimg.png", "PNG")
---
+++ geomath.py
@@ -0,0 +1,58 @@
+from math import cos, pi
+
+EARTH_RADIUS = 6378137.0
+EQUATOR_CIRCUMFERENCE = 2 * pi * EARTH_RADIUS
+INITIAL_RESOLUTION = EQUATOR_CIRCUMFERENCE / 256.0
+ORIGIN_SHIFT = EQUATOR_CIRCUMFERENCE / 2.0
+
+def meters2lat(lat, meters):
+    dlat = (meters / 2) / EARTH_RADIUS
+    return lat + (dlat * (180 / pi)), lat - (dlat * (180 / pi))
+
+def meters2lon(lat, lon, meters):
+    dlon = (meters / 2) / (EARTH_RADIUS * cos(pi * lat / 180))
+    return lon + (dlon * (180 / pi)), lon - (dlon * (180 / pi))
+    
+def dm2decdeg(dm):
+    d = float(dm[0])
+    m = float(dm[1])
+    
+    d = d + (m / 60)
+    return d
+
+def dms2decdeg(dms):
+    d = float(dms[0])
+    m = float(dms[1])
+    s = float(dms[2])
+    
+    m = m + (s / 60)
+    d = d + (m / 60)     
+    return d
+
+def checkformat(x):
+    if len(x) == 3:
+        return 'dms'
+    if len(x) == 2:
+        return 'dm'
+    if len(x) == 1:
+        return 'd'
+
+def convert2decdeg(lat, lon):
+    
+    # Lattitude
+    if checkformat(lat) == 'd':
+        retlat = float(lat[0])
+    if checkformat(lat) == 'dm':
+        retlat = dm2decdeg(lat)
+    if checkformat(lat) == 'dms':
+        retlat = dms2decdeg(lat)
+        
+    # Lattitude
+    if checkformat(lon) == 'd':
+        retlon = float(lon[0])
+    if checkformat(lon) == 'dm':
+        retlon = dm2decdeg(lon)
+    if checkformat(lon) == 'dms':
+        retlon = dms2decdeg(lon)
+    
+    return retlat, retlon
